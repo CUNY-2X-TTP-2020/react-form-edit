8b960316cb5ca8b24241eaa7008a2d12ce4d72be
RickyC0626 <rickychon99@gmail.com>
2020-06-03 13:50:55 -0400

    Add title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App()
     return (
         <div className="App">
             <header className="App-header">
-                
+                <h1>Form Edit</h1>
             </header>
         </div>
     );
